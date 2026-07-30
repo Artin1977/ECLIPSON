e2a1160882da680b4fb558704e2ea87e8f64022b
artingpt <artintehrani1977@gmail.com>
2026-07-30 19:24:11 +0330

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ const GAPGPT_API_KEY = 'sk-c4Lsmm4OV6HU0YgwwF2kJrcc8fxAAG7NAW8hDe4FflPO7EE6';
 const VIP_PASSWORD = '197777';
 
 // محدودیت‌های توکن روزانه
-const NORMAL_DAILY_LIMIT = 10000; // محدودیت کاربران عادی
-const VIP_DAILY_LIMIT = 30000;    // محدودیت کاربران ویژه (VIP)
+const NORMAL_DAILY_LIMIT = 5000; // محدودیت کاربران عادی
+const VIP_DAILY_LIMIT = 20000;    // محدودیت کاربران ویژه (VIP)

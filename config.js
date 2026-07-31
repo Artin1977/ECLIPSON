1996ba53016173c648b3e671add329d0884932f4
artingpt <artintehrani1977@gmail.com>
2026-07-31 12:02:37 +0330

    Modify token limits and notification settings
    Updated daily token limits for user levels and adjusted notification message.

--- config.js
+++ config.js
@@ -18,8 +18,8 @@ window.PASSWORDS = {
 
 // محدودیت‌های توکن روزانه برای هر سطح
 window.LIMITS = {
-    'FREE': 500,       
-    'PLUS': 4000,      
+    'FREE': 1500,       
+    'PLUS': 5000,      
     'PRO': 13000,      
     'PRO_PLUS': 30000  
 };
